99e00087d26b1343d887e4c2f6d561247d217808
Jeff Deville <jeffdeville@gmail.com>
2013-12-18 19:58:16 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tagsaroundtheworld::Application.config.secret_key_base = 'c7ca076f40f798c73e6d85079585691c4f20bc8a2fdfa0320db3ebd0d8eaa09914e31feb59235bbfa387b8ef82bb0c6830e0aa92280747260c1987cc4ed37161'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tagsaroundtheworld::Application.config.session_store :cookie_store, key: '_tagsaroundtheworld_session'
